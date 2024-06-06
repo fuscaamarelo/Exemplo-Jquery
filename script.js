65a11dd49c2ec0c5b1f9635ba43dbc95b94aaa15
fuscaamarelo <bvilanova023@gmail.com>
2024-06-06 11:57:58 -0300

    quase deu certo

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    $("#esconder").click(function(){
+        $('.texto').hide('slow');
+    });
+
+    $("#exibir").click(function(){
+        $('.texto').show('fast');
+    });
+});
